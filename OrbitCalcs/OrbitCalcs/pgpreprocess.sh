cdd5c922bfc9d777bb9af6f053c20fbed8661781
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-02-23 19:23:02 +0000

    final fix for mooncalcs

--- OrbitCalcs/OrbitCalcs/pgpreprocess.sh
+++ OrbitCalcs/OrbitCalcs/pgpreprocess.sh
@@ -5,6 +5,7 @@ make
 ./pgsummary ./CometEls.txt ../planetpositions.js /opt/bitnami/apps/wordpress/htdocs/data/
 cd ..
 src/conjunctioncalcs 51.88 -1.31 3 2
-src/mooncalcs 5.188 -1.31 2
 cd src
 ./orbitcalcs 51.88 -1.31 `date +%Y%m%d`  ..
+./mooncalcs 5.188 -1.31 2
+mv mooncalcs.js /home/bitnami/data/
