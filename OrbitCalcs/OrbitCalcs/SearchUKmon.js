cdffb917cb67acf5ea5fd1747a54ed0cf0a990af
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2020-05-02 12:23:08 +0100

    created separate events and params for each search

--- OrbitCalcs/OrbitCalcs/SearchUKmon.js
+++ OrbitCalcs/OrbitCalcs/SearchUKmon.js
@@ -60,7 +60,7 @@ function queryS3()
 				console.log('SelectObjectContent completed');
 			}
 		}
-		//console.log(`${dta}`);
+		console.log(`${dta}`);
 		dta=dta.replace(" ","");
 		dta=dta.replace(/(\r\n|\n|\r)/gm,",");
 		console.log(`${dta}`);
@@ -74,13 +74,30 @@ function queryS3()
 		}
 		res=res.concat('</tr></table>');
 		document.getElementById("results").innerHTML = res;
-		//console.log(`${res}`);
+		console.log(`${res}`);
 	});
 	var client2 = new AWS.S3({apiVersion: '2006-03-01'});
 	var res2='<table><tr><td>Y</td><td>M</td><td>D</td><td>h</td><td>m</td><td>s</td><td>Mag</td><td>Dir</td><td>Alt</td><td>Ra</td><td>Dec</td><td>Camera</td></tr>';
 	srchkey='consolidated/P_';
 	dta='';
-	params.Key=srchkey.concat(yr).concat('-unified.csv');
+	var params2 = {
+		Bucket: 'ukmon-shared',
+		Key: '',
+		ExpressionType: 'SQL',
+		Expression: '',
+		InputSerialization: {
+			CSV: {
+				FileHeaderInfo: 'USE',
+				RecordDelimiter: '\n',
+				FieldDelimiter: ','
+			}
+		},
+		OutputSerialization: {
+			CSV: {}
+		}
+	};
+
+	params2.Key=srchkey.concat(yr).concat('-unified.csv');
 	selp1='select Y, \"M\", D, h, \"m\", s, Mag, Az1, Alt1, Ra1, Dec1, ID ';
 	selp1=selp1.concat('from s3object q where ');
 	selp1=selp1.concat('cast(Y as int)=').concat(yr);
@@ -89,26 +106,26 @@ function queryS3()
 	selp1=selp1.concat(' and cast(h as int)=').concat(hr);
 	selp1=selp1.concat(' and cast(\"m\" as int)=').concat(mn);
 	console.log(`${selp1}`);
-	params.Expression= selp1;
-	client2.selectObjectContent(params, (err, data) => {
-		if (err) {
-			switch (err.name) {
+	params2.Expression= selp1;
+	client2.selectObjectContent(params2, (err2, data2) => {
+		if (err2) {
+			switch (err2.name) {
 			// Check against specific error codes that need custom handling
 			}
 			return;
 		}
 
 		// data.Payload is a Readable Stream
-		const events = data.Payload;
-		for (const event of events) {
-			if (event.Records) {
+		const events2 = data2.Payload;
+		for (const event2 of events2) {
+			if (event2.Records) {
 				// event.Records.Payload is a buffer containing
 				// a single record, partial records, or multiple records
-				dta=dta.concat(event.Records.Payload.toString());
+				dta=dta.concat(event2.Records.Payload.toString());
 				dta=dta.concat(',');
-			} else if (event.Stats) {
-				console.log(`Processed ${event.Stats.Details.BytesProcessed} bytes`);
-			} else if (event.End) {
+			} else if (event2.Stats) {
+				console.log(`Processed ${event2.Stats.Details.BytesProcessed} bytes`);
+			} else if (event2.End) {
 				console.log('SelectObjectContent completed');
 			}
 		}
@@ -126,7 +143,7 @@ function queryS3()
 		}
 		res2=res2.concat('</tr></table>');
 		document.getElementById("results2").innerHTML = res2;
-		//console.log(`${res}`);
+		console.log(`${res2}`);
 	});
 }
 
